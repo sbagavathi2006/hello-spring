fbb2114cb47951fef01758aa801599bd522ecd5e
Bagavathi <sbagavathi2006@gmail.com>
2023-09-20 21:58:41 -0500

    Controllers and Path Parameters

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,20 +2,34 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-
-        return "Hello, Spring!";
-    }
+//Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String helloPath(){
+//
+//        return "Hello, Spring!";
+//    }
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye(){
         return "GoodBye, Spring!";
+    }
+//Handle request of the form /hello?name=launchcode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+    return "Hello, "+name+"!";
+    }
+//Handles requests of the form /hello/launchcode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+    return "Hello, "+name+ "!";
     }
 }
