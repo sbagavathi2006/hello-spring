305075e67cf76fe2a47f5761188c667c9a42b733
Bagavathi <sbagavathi2006@gmail.com>
2023-10-05 22:43:54 -0500

    Completed

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,40 +3,52 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
+    @GetMapping("{n}")
+    @RequestMapping(method ={RequestMethod.GET,RequestMethod.POST},value = "createMessage")
+    @ResponseBody
+    public String createMessage(@RequestParam String n , @RequestParam String l){
+        String greet = "";
 
-    //handle request at path //http://localhost:8080/hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
+        if(l.equals("english")){
+            greet = "Hello";
+        } else if (l.equals("french")) {
+            greet = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greet = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greet = "Hola";
+        }
+        else if (l.equals("german")) {
+            greet = "Hallo";
+        }
+
+        return greet + " " + (n.isEmpty()?"World":n);
     }
+    @GetMapping(value="exform")
+    @ResponseBody
+    public String exerciseController() {
 
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
-    }
-
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("hello/{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
-    }
-
-    @GetMapping("form")
-    public String helloForm() {
-        return "<html>" +
+        String html ="<html>" +
                 "<body>" +
-                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
+                "<form action = 'createMessage' method = 'post'>" +
+                "<input type = 'text' name = 'n'/>" +
+                "<select name='l'>"+
+                "<option value='english'>English</option>"+
+                "<option value='french'>French</option>"+
+                "<option value='italian'>Italian</option>"+
+                "<option value='spanish'>Spanish</option>"+
+                "<option value='german'>German</option>"+
+                "</select>"+
+                "<input type = 'submit' value = 'Greet Me!'/>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+        return html;
     }
 
-}
+   }
